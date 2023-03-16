be8f068705fb0624e666ef33ec1ad4b4fd19f742
ravleen29 <114485306+ravleen29@users.noreply.github.com>
2023-03-16 11:19:36 +0000

    Student grade and name displayed

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using ConsoleAppProject.Helpers;
@@ -11,46 +12,66 @@ namespace ConsoleAppProject.App03
     /// </summary>
     public class StudentGrades
     {
-        public string FullName;
+        public string FirstName;
         public string LastName;
         public int Grades;
         public string A, B, C, D, F;
         public const int Max_St = 10;
         public void run()
         {
-            Console.WriteLine("Student Grades application!");
-            Result();
+            Console.WriteLine("    Student Grades application!   ");
+            ConnvertToGrades();
             Name();
+            GetStudentGrades();
         }
 
-        public string Result()
+        public string ConnvertToGrades()
         {
-            if (Grades >= 70)
+            switch (Grades)
             {
-                return A;
+                case int n when (n >= 70):
+                    return "A";
+                case int n when (n >= 60):
+                    return "B";
+                case int n when (n >= 50):
+                    return "C";
+                case int n when (n >= 40):
+                    return "D";
+                case int n when (n >= 0):
+                    return "F";
+                default:
+                    return "Invalid grade";
             }
-            else if (Grades >= 60)
-            {
-                return B;
-            }
-            else if (Grades >= 50)
-            {
-                return C;
-            }
-            else if (Grades >= 40)
-            {
-                return D;
-            }
-            else if (Grades >= 0)
-            {
-                return F;
-            }
-            return null;
         }
 
         public string Name()
         {
-            return $"{FullName}{LastName}";
+            return $"{FirstName} {LastName}";
         }
+
+        public void GetStudentGrades()
+        {
+            List<StudentGrades> studentGradesList = new List<StudentGrades>();
+            for (int i = 0; i < Max_St; i++)
+            {
+                Console.WriteLine("Please enter student name: ");
+                string name = Console.ReadLine();
+                
+                StudentGrades sg = new StudentGrades()
+                {
+                    FirstName = name,
+                };
+                Console.WriteLine("Please enter student grades: ");
+                int grades = Convert.ToInt32(Console.ReadLine());
+                sg.Grades = grades;
+                studentGradesList.Add(sg);
+            }
+            foreach (StudentGrades sg in studentGradesList)
+            {
+                Console.WriteLine($"{sg.Name()} Grade: {sg.ConnvertToGrades()}");
+            }
+        }
+
+
     }
 }
