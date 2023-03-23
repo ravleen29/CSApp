1a2ad3e666f577da34022dad25e91f2ae65e9e7c
ravleen29 <114485306+ravleen29@users.noreply.github.com>
2023-03-23 14:01:05 +0000

    App04

--- App04_web/Program.cs
+++ App04_web/Program.cs
@@ -1,36 +1,65 @@
 using Microsoft.EntityFrameworkCore;
 using App04_web.Data;
+using System.Drawing.Text;
+using Microsoft.EntityFrameworkCore.Migrations;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-//check database
-builder.Services.AddDbContext<AppDB>(options => options.UseSqlServer
-(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace App04_web.Models 
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 
-app.UseRouting();
+            //check database
+            builder.Services.AddDbContext<AppDB>(options => options.UseSqlServer
+            (builder.Configuration.GetConnectionString("DefaultConnection")));
 
-app.UseAuthorization();
+            var app = builder.Build();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+        private void CreateDbIfNotExists(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDB>();
+                    context.Database.EnsureCreated();
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured while creating Database!");
+                }
+            }
+        }
+    }
+  }
